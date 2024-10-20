a234c2237dfa637cbb517dcf7319a675855c486f
Vegeta-7 <soodakshat8@gmail.com>
2024-10-20 16:20:41 +0530

    --v2

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use('/user',userRoutes);
 const CONNECTION_URL = process.env.MONGO_URL
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
 mongoose.connect(CONNECTION_URL)
     .then(()=>{app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`))})
     .catch((error)=>{console.log(error.message)})
